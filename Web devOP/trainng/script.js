5216c10754ffe734b73779dfa984c233bbd7a51c
sakaMicro <saka.microverse@gmail.com>
2020-08-27 03:27:50 +0300

    all files

---
+++ Web devOP/trainng/script.js
@@ -0,0 +1,10 @@
+var b = 'smoothie';
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+var age = prompt('what is your age');
+
+document.getElementById('someText').innerHTML = "hello";
+
